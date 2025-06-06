0ea12b95646166d83fc311c16e9c0129caedc3a3
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-06-06 11:03:41 +0200

    init vue project + local hello world

---
+++ frontend/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+    <div class="hello">
+      <h1>Hello World</h1>
+    </div>
+
+</template>
+
+<style scoped>
+.hello {
+  text-align: center;
+  margin-top: 100px;
+  font-size: 2rem;
+}
+</style>
