58200f194f3720e2dfa1ed1f697d040d52a2a39a
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-06-11 00:49:22 +0200

    feat: add a homepage

---
+++ frontend/src/pages/HomePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Interactomes !</h1>
+    <UploadPanel />
+  </div>
+</template>
+
+<script setup>
+import UploadPanel from '@/components/UploadPanel.vue'
+</script>
+
+<style scoped>
+.home {
+  text-align: center;
+  padding: 3rem;
+}
+</style>
