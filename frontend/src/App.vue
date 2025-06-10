eb51cc74289e3067da0a49385d8221aecb564893
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-06-10 14:16:31 +0200

    refractor: smooth App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,18 +1,11 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
-    <div class="hello">
-      <h1>Hello World</h1>
-    </div>
-
+    <router-view/>
 </template>
 
 <style scoped>
-.hello {
-  text-align: center;
-  margin-top: 100px;
-  font-size: 2rem;
-}
+
 </style>
