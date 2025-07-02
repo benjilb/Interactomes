61218ddcd5dbadee6bee89e88b4e5d891109a03d
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-07-02 11:28:24 +0200

    feat: add gene name organism name and ID + some CSS

--- frontend/src/components/ProteinInfo.vue
+++ frontend/src/components/ProteinInfo.vue
@@ -4,6 +4,9 @@
     <ul>
       <li><strong>Uniprot ID:</strong> {{ protein.uniprot_id }}</li>
       <li><strong>Name:</strong> {{ protein.protein_name }}</li>
+      <li><strong>Gene:</strong> {{ protein.gene_name }}</li>
+      <li><strong>Organism:</strong> {{ protein.organism }}</li>
+      <li><strong>Organism ID:</strong> {{ protein.organism_id }}</li>
       <li><strong>Sequence:</strong>
         <pre class="sequence">{{ protein.sequence }}</pre>
       </li>
@@ -22,17 +25,29 @@ defineProps({
 
 <style scoped>
 .protein-info {
-  border-right: 1px solid #ccc;
   padding: 1rem;
   width: 300px;
   overflow: auto;
+  margin-top: 30px;
+}
+
+.protein-info ul {
+  list-style: none;
+  padding:0;
+  margin: 0;
+}
+
+.protein-info li {
+  margin-bottom: 0.5rem;
+  text-align: left; /* Sécurité supplémentaire pour les li */
 }
 
 .sequence {
   white-space: pre-wrap;
   word-break: break-word;
-  background: #f9f9f9;
   padding: 0.5rem;
   border-radius: 4px;
+
 }
+
 </style>
