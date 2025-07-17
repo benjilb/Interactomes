99e77968ea486a93d99829b39c2356ef3b738f8d
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-07-17 15:53:52 +0200

    refractor:import PrimeVue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,10 +4,13 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import './assets/global.css'
+import PrimeVue from 'primevue/config';
 
-const app = createApp(App)
 
-app.use(createPinia())
-app.use(router)
 
-app.mount('#app')
+const app = createApp(App);
+
+app.use(createPinia());
+app.use(router);
+app.use(PrimeVue);
+app.mount('#app');
