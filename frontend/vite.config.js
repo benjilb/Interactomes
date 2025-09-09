04eb9e7ac7687506e049e437e4f181e07effeabb
Benjamin Le Blanc <benjamin.le-blanc@univ-tlse3.fr>
2025-09-09 18:31:21 +0200

    refractor: add interactomes.scicore.unibas.ch to vute.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,22 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
 import { fileURLToPath, URL } from 'url'
-import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ['interactomes.scicore.unibas.ch'],
+  },
+  preview: {
+    allowedHosts: ['interactomes.scicore.unibas.ch'],
   }
 })
